a77ee9963e1f23fdf7e81d6a9289a6b5aa62b173
antoniosumak <sumakantonio@gmail.com>
2021-04-27 12:54:33 +0200

    Promjenjen button, dodan u generalStyles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import './Button.scss';
-const Button = ({
-    text
-}) => {
-    return (
-        <button className="Button">{ text }</button>
-    );
-}
+import React from "react";
+import { ButtonStyle } from "../../lib/style/generalStyles";
+const Button = ({ text }) => {
+  return <ButtonStyle>{text}</ButtonStyle>;
+};
 export default Button;
--- src/lib/style/generalStyles.js
+++ src/lib/style/generalStyles.js
@@ -40,3 +40,28 @@ export const FlexRow = styled.div`
     justify-content: space-between;
   }
 `;
+
+export const ButtonStyle = styled.button`
+  display: block;
+  text-decoration: none;
+  width: 100%;
+  line-height: 40px;
+  text-align: center;
+  border: 1px solid ${colors.red};
+  border-radius: 6px;
+  background: ${colors.red};
+  color: ${colors.white};
+  transition: ${transitionEase};
+  font-size: 16px;
+  font-family: "Montserrat", sans-serif;
+
+  &:hover {
+    cursor: pointer;
+    border-color: ${colors.yellow};
+    background: ${colors.yellow};
+  }
+
+  &:focus {
+    outline: none;
+  }
+`;
