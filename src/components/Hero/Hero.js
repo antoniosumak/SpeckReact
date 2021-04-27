d4c4c0ddca3ec62c2d0d5026c54a93fe10730f0b
antoniosumak <sumakantonio@gmail.com>
2021-04-27 13:21:25 +0200

    Promjenjen Hero, pretvoreno iz scss u styled comp

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,20 +1,30 @@
 import React from "react";
-import "./Hero.scss";
-
+import {
+  SectionHero,
+  SectionHeroOverlay,
+  SectionHeroContent,
+  SectionHeroHeading,
+  SectionHeroSubheading,
+} from "./HeroStyles";
 const Hero = () => {
-    return ( 
-        <section className="SectionHero">
-                <div className="SectionHero-Overlay">
-                    <div className="SectionHero-Content">
-                        <h1 className="SectionHero-Heading">
-                            Centar za podršku studentima i razvoj karijera na FOI</h1>
-                        <p className="SectionHero-Subheading">
-                            CPSRK aktivno radi na poboljšanju kvalitete cjelokupnog iskustva studiranja za studente i studentice FOI-ja na način da ih podržava, motivira i karijerno usmjerava. Svojim aktivnostima pomaže im kako bi se uspješno nosili s akademskim izazovima, napredovali kroz studij i počeli graditi svoju karijeru već tijekom studentskih dana.
-                        </p>
-                    </div>
-                </div>
-        </section>
-     );
-}
- 
+  return (
+    <SectionHero>
+      <SectionHeroOverlay>
+        <SectionHeroContent>
+          <SectionHeroHeading>
+            Centar za podršku studentima i razvoj karijera na FOI
+          </SectionHeroHeading>
+          <SectionHeroSubheading>
+            CPSRK aktivno radi na poboljšanju kvalitete cjelokupnog iskustva
+            studiranja za studente i studentice FOI-ja na način da ih podržava,
+            motivira i karijerno usmjerava. Svojim aktivnostima pomaže im kako
+            bi se uspješno nosili s akademskim izazovima, napredovali kroz
+            studij i počeli graditi svoju karijeru već tijekom studentskih dana.
+          </SectionHeroSubheading>
+        </SectionHeroContent>
+      </SectionHeroOverlay>
+    </SectionHero>
+  );
+};
+
 export default Hero;
